d6954f5511377f5a02a067ae9a1d0ecf1c8047ef
CommonSniper <yunsanc@gmail.com>
2026-07-23 10:15:26 +0900

    Local fix, 스킬 파란 글씨 추가

--- localization.lua
+++ localization.lua
@@ -1,5 +1,12 @@
 local mod_path = ModPath
+
 Hooks:Add("LocalizationManagerPostInit", "StableShotLocalization", function(self)
-	self:load_localization_file(mod_path .. "locale/en.json")
-	self:load_localization_file(mod_path .. "locale/ko.json")
+	local language_key = SystemInfo:language():key()
+	local locale_file = "en.json"
+
+	if language_key == "korean" or PackageManager:loaded("core/packages/language_korean") then
+		locale_file = "ko.json"
+	end
+
+	self:load_localization_file(mod_path .. "locale/" .. locale_file)
 end)
